129041073c2f357ba02e27d128af0797e9dab2c8
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-07-30 16:09:22 -0500

    functions done, TODO: strip quotes

--- code/parse.py
+++ code/parse.py
@@ -16,31 +16,83 @@ subset = data[['event', 'venue', 'place', 'date']]
 '''Functions to operate on each column and return cleaned data'''
 
 def clean_event(event):
+    # Work on this to remove all quotes
+    event = event.str.replace('"', '', regex=True)
+    event = event.str.replace('""', '', regex=True)
+    event = event.str.lstrip('"')
+    event = event.str.rstrip('"')
+
+
     event = event.str.replace('[', '', regex=True)
     event = event.str.replace(']', '', regex=True)
 
-    # TODO: Change this to replace with space if at end of word, : if in the middle
-    event = event.str.replace(';', '', regex=True)
-    return print(event.head(50))
+    # Match this: (?)
+    pattern1 = '\(\?\)'
+    event = event.str.replace(pattern1, '', regex=True)
+
+    # Match semicolons at the end of the string
+    pattern2 = '\;$'
+    event = event.str.replace(pattern2, '', regex=True)
+
+    # Match semicolons in the middle of a string and replace with :
+    pattern3 = '\\b;'
+    event = event.str.replace(pattern3, ':', regex=True)
+    return event.to_csv("../data/test_event.csv")
 
 def clean_venue(venue):
-    return print(venue.head())
+    venue = venue.str.replace(';', '')
+
+    venue = venue.str.replace('?', '')
+
+    venue = venue.str.replace('[', '', regex=True)
+    venue = venue.str.replace(']', '', regex=True)
+
+    venue = venue.str.replace('\(\)', '', regex=True)
+
+    return venue.to_csv("../data/test_venue.csv")
 
 def clean_place(place):
-    return print(place.head())
+    place = place.str.replace('"', '', regex=True)
+    place = place.str.replace('""', '', regex=True)
+    place = place.str.lstrip('"')
+    place = place.str.rstrip('"')
+
+    place = place.str.replace(';', '')
+
+    place = place.str.replace('?', '')
+
+    place = place.str.replace('[', '', regex=True)
+    place = place.str.replace(']', '', regex=True)
+
+    place = place.str.replace('\(', '', regex=True)
+    place = place.str.replace('\)', '', regex=True)
+
+    return place.to_csv("../data/test_place.csv")
 
 def clean_date(date):
-    return print(date.head())
+    
+    date = pd.Series([d for d in date if d != '190-03-06' or d != '1091-01-27' or d != '2928-03-26'])
+    
+    return date.to_csv("../data/test_date.csv")
 
+def merge_series_to_df(event, venue, place, date):
+
+    all_series = {"event": event, "venue": venue, "place": place, "date": date}
+    
+    df = pd.concat(all_series, axis=1)
+
+    return df.to_csv("../data/cleaned-NYPL-menus.csv")
 
 def main():
-    clean_event(event)
+    # clean_event(event)
 
     # clean_venue(venue)
 
     # clean_place(place)
 
-    # clean_date(date)
+    clean_date(date)
+
+    # merge_series_to_df(event, venue, place, date)
 
 
 if __name__ == "__main__":
